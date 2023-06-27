5b79a963643735c01815716af64f961f90d50b0a
usama shakeel <princeusamaansari15@gmail.com>
2023-06-27 17:43:40 +0500

    init

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'expo-dev-client';
+import React, { useState } from 'react';
+import AgoraUIKit, { PropsInterface } from 'agora-rn-uikit';
 
-export default function App() {
+const App = () => {
+  const connectionData = {
+    appId: 'd23dfacb5bb24d3ca261e5367897e4b7',
+    channel: 'n3',
+    token: '007eJxTYLCra1c9O1Xb8M3bv3sev5ko3KcYtmwOy0qpb7e2vgqMbHBTYEgxMk5JS0xOMk1KMjJJMU5ONDIzTDU1NjO3sDRPNUkyF7k+K6UhkJFB4a8uCyMDBIL4TAx5xgwMAJc9H/s=', // enter your channel token as a string 
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <AgoraUIKit connectionData={connectionData} />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
